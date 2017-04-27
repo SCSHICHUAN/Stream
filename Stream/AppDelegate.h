fed3e1b74f56917b5d4b0c977efe3e772f80e1a9
SHICHUAN <15180796292@163.com>
2017-04-27 10:16:15 +0800

    Initial Commit

---
+++ Stream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stream
+//
+//  Created by SHICHUAN on 2017/4/27.
+//  Copyright © 2017年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stream/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Stream
+//
+//  Created by SHICHUAN on 2017/4/27.
+//  Copyright © 2017年 SHICHUAN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
